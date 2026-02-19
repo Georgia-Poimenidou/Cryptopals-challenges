9f685b6fc6031632118227c8515aaa92c79f5e91
Georgia Poimenidou <ics23097@uom.edu.gr>
2026-02-19 10:39:36 +0200

    Refactor and duplicate code for Challenge 4

--- set1/challenge3.py
+++ set1/challenge3.py
@@ -1,4 +1,4 @@
-#Set 1, Challenge 3
+#Challenge 4 (S1C3)
 
 hex_string = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 binary_string = bytes.fromhex(hex_string)
@@ -37,5 +37,6 @@ def find_text(binary_text):
     print('The key is ' + chr(best_key) + ' and the XORed string is: ' + best_text)   
     
 find_text(binary_string)
+
 
 
